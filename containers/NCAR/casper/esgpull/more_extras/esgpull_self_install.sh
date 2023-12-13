25eff43e96a5e364ab187bba6acf04904b8148df
Benjamin Kirk <benkirk@ucar.edu>
2023-12-13 11:49:39 -0700

    esgpull seems to be working

--- containers/NCAR/casper/esgpull/more_extras/esgpull_self_install.sh
+++ containers/NCAR/casper/esgpull/more_extras/esgpull_self_install.sh
@@ -7,22 +7,26 @@ ESGPULL_INST_PATH=$1
 which esgpull || exit 1
 
 set -x
+timestamp="$(date +%F@%H:%M)"
+conffile="${HOME}/.config/esgpull/installs.json"
+
+# remove any existing config
+[ -f ${conffile} ] && mv ${conffile} ${conffile}.old.${timestamp}
+
 esgpull --version
 esgpull self install -n host ${ESGPULL_INST_PATH}
 
-esgpull config api.index_node esgf-node.llnl.gov
+esgpull config api.index_node "esgf-node.llnl.gov"
 
-esgpull config paths.data /glade/collections/cmpipfoo/
+esgpull config paths.data "/glade/derecho/scratch/benkirk/esgpull_repo"
+
+esgpull config download.max_concurrent 12
 
 echo 'y' | esgpull config --generate
+
+# give the log directory tmpdir-like permissions
+chmod 1777 ${ESGPULL_INST_PATH}/log && ls -ld ${ESGPULL_INST_PATH}/log
 
 esgpull search project:CMIP6 variable_id:tas experiment_id:'ssp*' member_id:r1i1p1f1 frequency:mon --detail 0
 
 esgpull config
-
-#mkdir -p ${ESGPULL_INST_PATH}/etc
-#cd ${ESGPULL_INST_PATH}
-#mv config.toml config_container.toml
-#cp config_container.toml config_host.toml
-#sed -i 's/container/host/g' config_host.toml
-#ln -s config_container.toml config.toml
