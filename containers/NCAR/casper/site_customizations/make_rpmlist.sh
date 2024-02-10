eba9b8fc6547c57bfc386f7aed222bfe74d55ed8
Benjamin Kirk <benkirk@ucar.edu>
2024-02-10 16:00:12 -0700

    conform to WRFV3 expected path...

--- containers/NCAR/casper/site_customizations/make_rpmlist.sh
+++ containers/NCAR/casper/site_customizations/make_rpmlist.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 
+# https://possiblelossofprecision.net/?p=1300
+# http://ftp.rpm.org/api/4.4.2.2/queryformat.html
+# rpm -qa --queryformat "%-60{Name} %{Vendor}\n"
+# rpm -qa --queryformat "%{NAME}-%{VERSION} %80{Vendor}\n"
+
 cat <<EOF > rpmlist.tmp
-$(ssh casper.hpc.ucar.edu rpm -qa 2>/dev/null || echo "Cannot connect")
+$(ssh casper.hpc.ucar.edu 'rpm -qa --queryformat "%{Name},%{Vendor}\n"' 2>/dev/null || echo "Cannot connect")
 EOF
 
 grep "Cannot connect" rpmlist.tmp && exit 1
+
+cat rpmlist.tmp | sort > rpmlist.tmp2 && mv rpmlist.tmp{2,}
+cat rpmlist.tmp | grep -i "SUSE" > rpmlist.tmp2 && mv rpmlist.tmp{2,}
+cat rpmlist.tmp | cut -d',' -f1 > rpmlist.tmp2 && mv rpmlist.tmp{2,}
+
+mv rpmlist.tmp rpmlist
+cat rpmlist
+
+
 
 exit 0
